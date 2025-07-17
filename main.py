3cdea5e1117d1db6dfca223e7df57c744dec5f7a
Pablus Vinii <pablus.vinicius@live.com>
2025-07-17 12:48:43 -0300

    On branch main 	modified:   main.py

--- main.py
+++ main.py
@@ -10,13 +10,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
-app = FastAPI(
-    print("FastAPI inicializado")
-    title="IA JW API",
-    version="1.0",
-    docs_url="/docs",       # Isso ativa o Swagger UI
-    redoc_url="/redoc",     # Isso ativa o Redoc (opcional)
-)
+app = FastAPI()
+print("FastAPI inicializado")
 
 # 🔐 Habilita CORS para qualquer origem (ou especifique seu domínio do Vercel)
 app.add_middleware(
